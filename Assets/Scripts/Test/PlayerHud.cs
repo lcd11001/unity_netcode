b022106283e3373bd1fb8b2704e4916a3938161b
Luong Cong Dan <dan.luongcong@gameloft.com>
2023-01-19 07:32:07 +0700

    * player armature network prefab + Player hud

---
+++ Assets/Scripts/Test/PlayerHud.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHud : NetworkBehaviour
+{
+    [SerializeField] TextMeshPro playerInfo;
+    private NetworkVariable<string> playerName = new NetworkVariable<string>();
+    private bool overlaySet = false;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        if (IsServer)
+        {
+            playerName.Value = $"Player {OwnerClientId}";
+
+            SetOverlay();
+        }
+    }
+
+    private void SetOverlay()
+    {
+        playerInfo.text = playerName.Value;
+    }
+
+    private void Update()
+    {
+        playerInfo.transform.rotation = Camera.main.transform.rotation;
+    }
+}
