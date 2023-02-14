47b95b8e5225f375af8c9b0b7012d341c8cd46f1
LCD-PC <dan.luongcong@gameloft.com>
2023-02-14 10:32:40 +0700

    + heart beat lobby + join lobby

--- Assets/Scripts/Lobby/TestLobby.cs
+++ Assets/Scripts/Lobby/TestLobby.cs
@@ -9,9 +9,17 @@ using UnityEngine;
 
 public class TestLobby : CommandBehaviour
 {
+    private Lobby hostLobby;
+
+    [SerializeField]
+    private float heartBeatTimerMax = 15f;
+    private float heartBeatTimer;
+
     protected async override void Start()
     {
         base.Start();
+
+        heartBeatTimer = heartBeatTimerMax;
 
         await UnityServices.InitializeAsync();
 
@@ -23,6 +31,25 @@ public class TestLobby : CommandBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
+    private void Update()
+    {
+        HandleLobbyHeartBeat();
+    }
+
+    private async void HandleLobbyHeartBeat()
+    {
+        if (hostLobby != null)
+        {
+            heartBeatTimer -= Time.deltaTime;
+            if (heartBeatTimer < 0f)
+            {
+                heartBeatTimer = heartBeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
     [Command]
     private async void CreateLobby()
     {
@@ -32,7 +59,96 @@ public class TestLobby : CommandBehaviour
             int maxPlayers = 4;
 
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
-            Debug.Log($"Lobby created {lobby.Name} : {lobby.MaxPlayers}");
+            Debug.Log($"Lobby created [{lobby.Id}] : {lobby.Name} : {lobby.MaxPlayers} : {lobby.LobbyCode}");
+
+            hostLobby = lobby;
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void ListLobbiesByOptions(QueryLobbiesOptions options)
+    {
+        try
+        {
+            var response = await Lobbies.Instance.QueryLobbiesAsync(options);
+
+            Debug.Log($"Lobbies found {response.Results.Count}");
+            foreach (var lobby in response.Results)
+            {
+                Debug.Log($"{lobby.Name} : {lobby.MaxPlayers}");
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [Command]
+    private void ListLobbies()
+    {
+        try
+        {
+            ListLobbiesByOptions(null);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [Command]
+    private void ListLobbiesByName(string filterName)
+    {
+        try
+        {
+            QueryLobbiesOptions options = new QueryLobbiesOptions()
+            {
+                Count = 25,
+                Filters = new List<QueryFilter>
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT),
+                    new QueryFilter(QueryFilter.FieldOptions.Name, filterName, QueryFilter.OpOptions.CONTAINS)
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(true, QueryOrder.FieldOptions.AvailableSlots),
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+            };
+
+            ListLobbiesByOptions(options);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [Command]
+    private async void JoinLobbyById(string id)
+    {
+        try
+        {
+            var lobby = await Lobbies.Instance.JoinLobbyByIdAsync(id);
+            Debug.Log($"Joined to lobby {lobby.Name} : {lobby.AvailableSlots}");
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [Command]
+    private async void JoinLobbyByCode(string code)
+    {
+        try
+        {
+            var lobby = await Lobbies.Instance.JoinLobbyByCodeAsync(code);
+            Debug.Log($"Joined to lobby {lobby.Name} : {lobby.AvailableSlots}");
         }
         catch (LobbyServiceException e)
         {
