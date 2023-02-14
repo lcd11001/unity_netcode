28def5570f3d46ab9002532ff235c2e0e381c413
LCD-PC <dan.luongcong@gameloft.com>
2023-02-14 09:31:06 +0700

    + Test lobby

---
+++ Assets/Scripts/Lobby/TestLobby.cs
@@ -0,0 +1,42 @@
+using SmartConsole;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+
+public class TestLobby : CommandBehaviour
+{
+    protected async override void Start()
+    {
+        base.Start();
+
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log($"Signed in {AuthenticationService.Instance.PlayerId}");
+        };
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    [Command]
+    private async void CreateLobby()
+    {
+        try
+        {
+            string lobbyName = "MyLobby";
+            int maxPlayers = 4;
+
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+            Debug.Log($"Lobby created {lobby.Name} : {lobby.MaxPlayers}");
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
