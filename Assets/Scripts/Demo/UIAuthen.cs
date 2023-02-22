3f981cb7d048c875e57bd4ff2a2af6a1224da9fd
LCD-PC <dan.luongcong@gameloft.com>
2023-02-23 05:29:54 +0700

    * Change relay protocol at runtime

--- Assets/Scripts/Demo/UIAuthen.cs
+++ Assets/Scripts/Demo/UIAuthen.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Networking.Transport;
+using Unity.Networking.Transport.Relay;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,16 +10,42 @@ namespace Demo
 {
     public class UIAuthen : UIBase<UIAuthen>
     {
+        [SerializeField] Toggle chkRelay;
         [SerializeField] Button btnLogin;
 
         private void OnEnable()
         {
             btnLogin.onClick.AddListener(OnLoginClicked);
+            chkRelay.onValueChanged.AddListener(OnRelayChanged);
         }
 
         private void OnDisable()
         {
             btnLogin.onClick.RemoveListener(OnLoginClicked);
+            chkRelay.onValueChanged.RemoveListener(OnRelayChanged);
+        }
+
+        private void OnRelayChanged(bool enable)
+        {
+            chkRelay.GetComponentInChildren<Text>().text = enable ? "Relay" : "127.0.0.1:7777";
+            if (enable)
+            {
+                RelayManager.Instance.Transport.SetRelayServerData(default(RelayServerData));
+            }
+            else
+            {
+                RelayManager.Instance.Transport.SetConnectionData("127.0.0.1", 7777);
+            }
+
+            StartCoroutine(RefreshLayout(chkRelay));
+        }
+
+        private IEnumerator RefreshLayout(Toggle toggle)
+        {
+            yield return new WaitForEndOfFrame();
+            var layout = toggle.GetComponent<HorizontalLayoutGroup>();
+            layout.enabled = false;
+            layout.enabled = true;
         }
 
         private void OnLoginClicked()
